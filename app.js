98f03538152005bd1bb39a82d495d252afa797e2
ross_a <alextross15@gmail.com>
2024-01-03 13:56:37 -0500

    Add book form submision is working.

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const modal = document.querySelector('.book-modal');
 const openModal = document.querySelector('.open-modal');
 const closeModal = document.querySelector('.close-btn');
+const bookForm = document.querySelector('#book-form');
 const myLibrary = [];
 
 openModal.addEventListener('click', () => {
     modal.showModal();
 });
 
+bookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = Number(document.getElementById("pages").value);
+    let read = document.getElementById("read").value;
+
+    console.log(title, author, pages, read);
+});
+
 closeModal.addEventListener('click', () => {
     modal.close();
-})
+});
 
