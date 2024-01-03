628aa3946ebcc69a5011c066e3845e30ffd91930
ross_a <alextross15@gmail.com>
2024-01-03 15:56:12 -0500

    Save for transition to home workstation.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const modal = document.querySelector('.book-modal');
 const openModal = document.querySelector('.open-modal');
 const closeModal = document.querySelector('.close-btn');
 const bookForm = document.querySelector('#book-form');
+const gridContainer = document.querySelector('.grid-container');
 const myLibrary = [];
 
 openModal.addEventListener('click', () => {
@@ -10,13 +11,15 @@ openModal.addEventListener('click', () => {
 
 bookForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    modal.close();
+    bookForm.reset();
+
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let pages = Number(document.getElementById("pages").value);
     let read = document.getElementById("read").value;
-});
 
-closeModal.addEventListener('click', () => {
-    modal.close();
+    let newChild = document.createElement('div');
+    newChild.setAttribute('class', 'child')
+    gridContainer.appendChild(newChild);
 });
-
