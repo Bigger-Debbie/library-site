9db309b7cde02d54492bfd10c937f48a89bc19eb
ross_a <alextross15@gmail.com>
2024-01-04 16:06:18 -0500

    Book function and createBookCard function added to app.js.

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const openModal = document.querySelector('.open-modal');
 const closeModal = document.querySelector('.close-btn');
 const bookForm = document.querySelector('#book-form');
 const gridContainer = document.querySelector('.grid-container');
-const myLibrary = [];
+const books = [
+    {
+        'title': 'Lord of the Rings',
+        'by': 'J.R.R Tolkien',
+        'read': false,
+        'pages': 1216
+    }
+]
 
 openModal.addEventListener('click', () => {
     modal.showModal();
@@ -12,14 +19,58 @@ openModal.addEventListener('click', () => {
 bookForm.addEventListener('submit', (e) => {
     e.preventDefault();
     modal.close();
-    bookForm.reset();
 
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let pages = Number(document.getElementById("pages").value);
-    let read = document.getElementById("read").value;
+    let read = document.getElementById("read").checked;
+    
+    if (read) {
+        read = true;
+    } else {
+        read = false;
+    }
 
-    let newChild = document.createElement('div');
-    newChild.setAttribute('class', 'child')
-    gridContainer.appendChild(newChild);
+    let newBook = new Book(title, author, pages, read)
+    books.push(newBook);
+    console.log(books);
+    createBookCard(newBook);
+    bookForm.reset();   
 });
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function createBookCard(book) {
+    let card = document.createElement('div');
+    card.setAttribute('class', 'child');
+
+    let header = document.createElement('h1');
+    header.textContent = `${book.title}`;
+    let vertDiv = document.createElement('div');
+    vertDiv.setAttribute('class', 'vert-div');
+    let authInfo = document.createElement('h2');
+    authInfo.textContent = `By: ${book.author}`;
+    authInfo.setAttribute('class', 'info');
+    let readInfo = document.createElement('h2');
+    readInfo.textContent = `Read Before: ${book.read === true ? 'Yes' : 'No'}`;
+    readInfo.setAttribute('class', 'info');
+    let pageInfo = document.createElement('h2');
+    pageInfo.textContent = `Pages: ${book.pages}`;
+    pageInfo.setAttribute('class', 'info');
+    let resetBtn = document.createElement('button');
+    resetBtn.setAttribute('class', 'btn info-reset');
+    resetBtn.textContent = 'Read Reset';
+
+    card.appendChild(header);
+    card.appendChild(vertDiv);
+    card.appendChild(authInfo);
+    card.appendChild(readInfo);
+    card.appendChild(pageInfo);
+    card.appendChild(resetBtn);
+    gridContainer.appendChild(card);
+}
